7069fda450dbe5007eb5d3bcd41491b687614f6c
KayCCCCCC <nguyenngothanhnha2003kg@gmail.com>
2023-09-05 22:49:13 +0700

    handle delete all

--- client/src/script2.js
+++ client/src/script2.js
@@ -185,13 +185,17 @@
                 },
                 body: JSON.stringify(selectedIds), 
             })
-            .then(response => {
-                console.log(response.json())
-                if (response.ok) {
+            .then(response => response.json())
+            .then(data =>{
+                console.log(data)
+                if(data.err == 0){
                     console.log('Deleted project with ID:', selectedIds);
-                    location.reload();
-                } else {
+                }else{
                     console.error('Failed to delete project with ID:', selectedIds);
+                    showErrorToast1(data.mes);
+                    setTimeout(function() {
+                        location.reload();
+                    }, 3000);
                 }
             })
             .catch(error => {
@@ -366,6 +370,85 @@ function handleDeleteButtonClick(id, status) {
         console.log(`Dự án có id ${id} không thể xóa vì trạng thái không phải 'NEW'.`);
     }
 }
+
+
+
+// Toast function
+function toast({ title = "", message = "", type = "info", duration = 3000 }) {
+    const main = document.getElementById("toast");
+    if (main) {
+      const toast = document.createElement("div");
+  
+      // Auto remove toast
+      const autoRemoveId = setTimeout(function () {
+        main.removeChild(toast);
+      }, duration + 1000);
+  
+      // Remove toast when clicked
+      toast.onclick = function (e) {
+        if (e.target.closest(".toast__close")) {
+          main.removeChild(toast);
+          clearTimeout(autoRemoveId);
+        }
+      };
+  
+      const icons = {
+        success: "fas fa-check-circle",
+        info: "fas fa-info-circle",
+        warning: "fas fa-exclamation-circle",
+        error: "fas fa-exclamation-circle"
+      };
+      const icon = icons[type];
+      const delay = (duration / 1000).toFixed(2);
+  
+      toast.classList.add("toast", `toast--${type}`);
+      toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
+  
+      toast.innerHTML = `
+                      <div class="toast__icon">
+                          <i class="${icon}"></i>
+                      </div>
+                      <div class="toast__body">
+                          <h3 class="toast__title">${title}</h3>
+                          <p class="toast__msg">${message}</p>
+                      </div>
+                      <div class="toast__close">
+                          <i class="fas fa-times"></i>
+                      </div>
+                  `;
+      main.appendChild(toast);
+    }
+}
+
+function showSuccessToast() {
+    console.log(1)
+    toast({
+      title: "Success!",
+      message: "You have successfully created the project !!!",
+      type: "success",
+      duration: 3000
+    });
+  }
+
+  function showErrorToast() {
+    console.log(2)
+    toast({
+      title: "Failed!",
+      message: "Please to fill all required !!!",
+      type: "error",
+      duration: 3000
+    });
+  }
+
+  function showErrorToast1(message) {
+    console.log(2)
+    toast({
+      title: "Failed!",
+      message: message,
+      type: "error",
+      duration: 3000
+    });
+  }
 
 
 
